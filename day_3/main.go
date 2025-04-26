c66e34f5249047592049fff66c8d65ea3e1661cc
Edward Wang <wardzxzxc@gmail.com>
2025-04-26 19:43:59 +0800

    feat: day 3 part 2 + clean up unused func

--- day_3/main.go
+++ day_3/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"regexp"
 	"strconv"
@@ -11,99 +10,67 @@ import (
 
 func main() {
 	part1()
-}
-
-type DigitNumbers struct {
-	X int
-	Y int
-}
-
-func getDigitNumbers(startIdx int, runes []rune) (*DigitNumbers, error) {
-	var firstNumRunes []rune
-	validFirstNum := true
-	for i := 0; i < 4; i++ {
-		currentRune := runes[startIdx+i]
-
-		if i == 0 && !commons.IsNumeric(currentRune) {
-			validFirstNum = false
-			break
-		}
-
-		if i > 0 && !commons.IsNumeric(currentRune) && currentRune != ',' {
-			validFirstNum = false
-			break
-		}
-
-		if i > 0 && currentRune == ',' {
-			break
-		}
-
-		firstNumRunes = append(firstNumRunes, currentRune)
-	}
-
-	if !validFirstNum {
-		return nil, errors.New("Invalid First Digit Number")
-	} else {
-		secondIdx := startIdx + len(firstNumRunes) + 1
-		var secondNumRunes []rune
-		validSecondNum := true
-
-		for i := 0; i < 4; i++ {
-			currentRune := runes[secondIdx+i]
-
-			if i == 0 && !commons.IsNumeric(currentRune) {
-				validSecondNum = false
-				break
-			}
-
-			if i > 0 && !commons.IsNumeric(currentRune) && currentRune != ')' {
-				validSecondNum = false
-				break
-			}
-
-			if i > 0 && currentRune == ')' {
-				break
-			}
-
-			secondNumRunes = append(secondNumRunes, currentRune)
-		}
-		if validSecondNum {
-			secondNumStr := string(secondNumRunes)
-			secondNum, _ := strconv.Atoi(secondNumStr)
-			firstNumStr := string(firstNumRunes)
-			firstNum, _ := strconv.Atoi(firstNumStr)
-			return &DigitNumbers{firstNum, secondNum}, nil
-		} else {
-			return nil, errors.New("Invalid Second Digit Number")
-		}
-	}
+	part2()
 }
 
 func part1() {
 	scanner := commons.GetInputFileScannerPtr()
 	defer scanner.Close()
 
-	re := regexp.MustCompile(`mul\(`)
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
 	total := 0
 
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		matches := re.FindAllStringIndex(line, -1)
+		matches := re.FindAllStringSubmatch(line, -1)
 
 		for _, match := range matches {
-			firstIdx := match[1]
-			runes := []rune(line)
+			x, xErr := strconv.Atoi(match[1])
+			y, yErr := strconv.Atoi(match[2])
 
-			digitNumbersPtr, err := getDigitNumbers(firstIdx, runes)
-			if err != nil {
-				continue
-			} else {
-				total += digitNumbersPtr.X * digitNumbersPtr.Y
+			if xErr == nil && yErr == nil {
+				total += x * y
 			}
-
 		}
 	}
 
+	fmt.Println(total)
+}
+
+func part2() {
+	scanner := commons.GetInputFileScannerPtr()
+	defer scanner.Close()
+
+	re := regexp.MustCompile(`mul\((\d+),(\d+)\)|do\(\)|don't\(\)`)
+	total := 0
+	enable := true
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		matches := re.FindAllStringSubmatch(line, -1)
+
+		for _, match := range matches {
+			fullMatch := match[0]
+
+			if fullMatch == "do()" {
+				enable = true
+				continue
+			} else if fullMatch == "don't()" {
+				enable = false
+				continue
+			} else {
+				if enable {
+					xStr := match[1]
+					yStr := match[2]
+					x, xErr := strconv.Atoi(xStr)
+					y, yErr := strconv.Atoi(yStr)
+					if xErr == nil && yErr == nil {
+						total += x * y
+					}
+				}
+			}
+		}
+	}
 	fmt.Println(total)
 }
